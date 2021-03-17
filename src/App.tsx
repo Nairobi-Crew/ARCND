0e5308cef71db72f6e13524e759ecf903d7d1936
Nikolay Sidorov <sidorov.n@idaproject.com>
2021-03-17 05:02:39 +0300

    add stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React, { PureComponent } from 'react';
 
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Мой апп.</h1>
-        </div>
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Мой апп.</h1>
+      </div>
     );
-    }
+  }
 }
 export default App;
