24e55c310b0d0c36c4eb84f45adb170b250bcc38
Alexander Stenkin <sia@1gx.org>
2021-04-06 18:05:27 +0300

    fix duplicate

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './common/common.scss';
 
-function startServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/serviceWorker.js').then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch((error: string) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-    });
-  }
-}
-
-startServiceWorker();
-
 ReactDOM.render(
   <App />,
   document?.getElementById('root'),
