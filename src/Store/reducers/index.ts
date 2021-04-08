3c51153f85e3c2adb375dbf341b815f6ccf69ffa
Oleg Belonovsky <abelo77@yandex.ru>
2021-04-08 14:33:20 +0300

    Добавлен редюсер для состояния игры

--- src/Store/reducers/index.ts
+++ src/Store/reducers/index.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { authReducer } from 'Reducers/auth/auth';
 import { userReducer } from 'Reducers/user/user';
+import { gameReducer } from 'Reducers/game/game';
 
-export default combineReducers({ auth: authReducer, user: userReducer });
+export default combineReducers({ auth: authReducer, user: userReducer, game: gameReducer });
--- src/Store/types.ts
+++ src/Store/types.ts
@@ -1,16 +1,19 @@
 import { defaultAuthReducer, IAuthUserReducer } from 'Reducers/auth/auth';
 import { defaultUserReducer, IUserReducer } from 'Reducers/user/user';
+import { defaultGameReducer, IGameReducer } from 'Reducers/game/game';
 
 export type Nullable<T> = T | null;
 
 export interface IAppState {
   auth: IAuthUserReducer,
   user: IUserReducer,
+  game: IGameReducer,
 }
 
 export const initialAppState: IAppState = {
   auth: defaultAuthReducer,
   user: defaultUserReducer,
+  game: defaultGameReducer,
 };
 
 export interface IUser {
