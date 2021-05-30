0403a7df7b64ff7549b78b9130fc3fc4f0337e98
Nikolay Sidorov <sidorov.n@idaproject.com>
2021-05-30 04:10:23 +0300

    add layout component

--- src/server/renderApp.tsx
+++ src/server/renderApp.tsx
@@ -1,45 +1,19 @@
-import React, {FC, useState} from 'react';
+import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import routes from 'Config/routes';
-import {useThemeReselect} from "Store/hooks";
-import Form from "UI/Form";
-import Input from "UI/Input";
-import Button from "UI/Button";
+import Layout from "UI/Layout";
 
 const renderApp = () => {
   return (
-    <App />
-  )
-};
-
-const App: FC = () => {
-  const theme = useThemeReselect()
-  const [formVisible,setFormVisible] = useState(false)
-  const [name,setName] = useState('')
-  const [message,setMessage] = useState('')
-  return (
-    <div className={`root${theme&&` root_theme_${theme.theme.color}`}`}>
+    <Layout>
       <Switch>
         {routes.map(
-          (route) => <Route {...route} />,
+          (route) => <Route showLayout={route.showLayout ?? true} {...route} />,
         )}
       </Switch>
-      <Button className="root__bnt" onClick={()=>{
-        setFormVisible(true)
-      }}>Напиште нам
-      </Button>
-      {formVisible&&<div className="root__form">
-        <Button className="root__form-close" onClick={()=>setFormVisible(false)}>
-          х
-        </Button>
-        <Form caption='Обратная связь' onSubmit={()=>setFormVisible(false)}>
-          <Input label="Ваше имя" value={name} onValueChanged={(v) => setName(v)} />
-          <Input label="Сообщение" value={message} onValueChanged={(v) => setMessage(v)} />
-        </Form>
-      </div>}
-    </div>
+    </Layout>
   )
-}
+};
 
 export default renderApp;
 
