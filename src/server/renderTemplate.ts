32ee17582c0345ce0fb9f5f5425985c21c6a6e19
oleg <abelo77@yandex.ru>
2021-06-05 11:09:04 +0300

    Добавлено: SEO Optimization

--- src/server/renderTemplate.ts
+++ src/server/renderTemplate.ts
@@ -41,6 +41,7 @@ startServiceWorker();
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Yandex Praktikum Middle frontend project. Arcanoid game. Nairobi crew. Aleh Belanouski & Nikolay Sidorov">
         ${useCSP}
         <link rel="stylesheet" href="/${cssPath}" />
 
