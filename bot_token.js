e8ca6698f8e47b8776ca5f7acb60ca76cb888080
oleg <abelo77@yandex.ru>
2021-06-05 12:10:31 +0300

    Добавлено: SEO Optimization

--- bot_token.js
+++ bot_token.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-underscore-dangle
-const _bot_token_ = 'bot1844914670:AAG3e0sMzDJvgGh4LRFJG9KVPcDmMGke1MM';
+const _bot_token_ = '1844914670:AAG3e0sMzDJvgGh4LRFJG9KVPcDmMGke1MM';
 
 module.exports = {
   _bot_token_,
--- src/server/renderTemplate.ts
+++ src/server/renderTemplate.ts
@@ -42,6 +42,7 @@ startServiceWorker();
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Yandex Praktikum Middle frontend project. Arcanoid game. Nairobi crew. Aleh Belanouski & Nikolay Sidorov">
+        <link rel="manifest" href="manifest.webmanifest">
         ${useCSP}
         <link rel="stylesheet" href="/${cssPath}" />
 
